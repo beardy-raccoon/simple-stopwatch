0a4cebd95f2bcc7da998cf5a5de1ce116f4f195c
Vlad Meshcherinov <vladislav1310@gmail.com>
2023-03-27 00:12:28 +0400

    feat: implement stopwatch and add button

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="page">
+    <main class="content">
+      <div v-for="(item, index) in stopwatches" :key="index">
+        <Stopwatch />
+      </div>
+      <div class="add-button-container">
+        <button @click="addStopwatch" class="button add-button"></button>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import Stopwatch from './components/Stopwatch-item.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Stopwatch,
+  },
+  data() {
+    const stopwatches = reactive([{}]);
+    const addStopwatch = () => {
+      stopwatches.push({});
+    };
+    return {
+      stopwatches,
+      addStopwatch,
+    };
+  },
+};
+</script>
+
+<style>
+@import url(./assets/vendor/normalize.css);
+@import url(./assets/vendor/fonts/fonts.css);
+
+#app {
+  font-family: 'Inter', 'Arial', sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  text-rendering: optimizeLegibility;
+  text-align: center;
+  color: #9E9E9E;
+  box-sizing: border-box;
+}
+
+.page {
+  height: 100vh;
+  background-color: #353638;
+  margin: 0 auto;
+}
+
+.content {
+  margin: 0 auto;
+  padding: 72px 212px;
+  max-width: 1200px;
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(3, 225px);
+  gap: 50px;
+}
+
+.button {
+  width: 20px;
+  height: 20px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  padding: 0;
+  border: unset;
+  cursor: pointer;
+  transition: opacity .9s;
+}
+
+.button:hover {
+  opacity: .5;
+  transition: opacity .3s;
+}
+
+.add-button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 225px;
+  height: 120px;
+  background-color: #696969;
+}
+
+.add-button {
+  background-image: url(./assets/images/add.svg);
+}
+
+@media screen and (max-width: 1023px) {
+  .content {
+    padding: 72px 135px;
+    flex-wrap: wrap;
+    grid-template-columns: repeat(2, 225px);
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .content {
+    padding: 72px 46px;
+    grid-template-columns: 225px;
+  }
+}
+</style>
---
+++ src/components/Stopwatch-item.vue
@@ -0,0 +1,124 @@
+<template>
+  <div :class="`stopwatch-container stopwatch-container${stopwatchState}`">
+    <div :class="`timetable timetable${stopwatchState}`">
+      <p v-if="time === 0" class="time">{{ time }}</p>
+      <p v-else-if="time > 0 && time < 60" class="time">{{ time }}</p>
+      <p v-else-if="time >= 60 && time < 3600" class="time">{{ `${getMinutes}:${getSeconds}` }}</p>
+      <p v-else-if="time >= 3600" class="time">{{ `${getHours}:${getMinutes}:${getSeconds}` }}</p>
+    </div>
+    <div class="buttons">
+      <button v-if="!isRunnig" @click="start" class="button start-btn"></button>
+      <button v-else @click="stop" :class="`button stop-btn${stopwatchState}`"></button>
+      <button @click="reset" :class="`button reset-btn${stopwatchState}`"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: 0,
+      isRunnig: false,
+      interval: null,
+    };
+  },
+  computed: {
+    stopwatchState() {
+      if (this.isRunnig) return '_active';
+      return '';
+    },
+    getHours() {
+      const hours = parseInt(this.time / 3600, 10);
+      return hours < 10 ? `0${hours}` : hours;
+    },
+    getMinutes() {
+      const minutes = parseInt(this.time / 60, 10) % 60;
+      return minutes < 10 ? `0${minutes}` : minutes;
+    },
+    getSeconds() {
+      const seconds = this.time % 60;
+      return seconds < 10 ? `0${seconds}` : seconds;
+    },
+  },
+  methods: {
+    start() {
+      this.isRunnig = true;
+      this.interval = setInterval(() => { this.time += 1; }, 1000);
+    },
+    stop() {
+      this.isRunnig = false;
+      clearInterval(this.interval);
+    },
+    reset() {
+      this.isRunnig = false;
+      clearInterval(this.interval);
+      this.time = 0;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.stopwatch-container {
+  min-width: 225px;
+  height: 120px;
+  background-color: #696969;
+  display: flex;
+  flex-direction: column;
+}
+
+.stopwatch-container_active {
+  color: #fff;
+}
+
+.timetable {
+  width: 100%;
+  min-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #9E9E9E;
+}
+
+.timetable_active {
+  border-color: #fff;
+}
+
+.time {
+  font-family: 'Gotham Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 21px;
+  min-width: 30px;
+  text-align: right;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60px;
+  gap: 52px;
+}
+
+.start-btn {
+  background-image: url(../assets/images/start.svg);
+}
+
+.stop-btn {
+  background-image: url(../assets/images/stop.svg);
+}
+
+.stop-btn_active {
+  background-image: url(../assets/images/stop-active.svg);
+}
+
+.reset-btn {
+  background-image: url(../assets/images/reset.svg);
+}
+
+.reset-btn_active {
+  background-image: url(../assets/images/reset-active.svg);
+}</style>
